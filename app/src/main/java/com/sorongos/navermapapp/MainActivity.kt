9a640f11e6f6bac358da886e31e4ca056274d63d
Sorongos <dosora9@gmail.com>
2023-03-24 14:18:07 +0900

    mapView를 이용해 지도 띄우기

--- app/src/main/java/com/sorongos/navermapapp/MainActivity.kt
+++ app/src/main/java/com/sorongos/navermapapp/MainActivity.kt
@@ -2,10 +2,51 @@ package com.sorongos.navermapapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sorongos.navermapapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mapView.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 }
