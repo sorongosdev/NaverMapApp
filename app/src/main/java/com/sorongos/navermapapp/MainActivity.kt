d3395dbb52c39a837f5c91ea0edd3b946c417427
Sorongos <dosora9@gmail.com>
2023-03-24 20:07:38 +0900

    쿼리 검색 후 마커 찍기

--- app/src/main/java/com/sorongos/navermapapp/MainActivity.kt
+++ app/src/main/java/com/sorongos/navermapapp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.sorongos.navermapapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.naver.maps.geometry.LatLng
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import com.naver.maps.geometry.Tm128
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.Marker
 import com.sorongos.navermapapp.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -15,7 +18,7 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var naverMap : NaverMap
+    private lateinit var naverMap: NaverMap
     private var isMapInit = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,19 +29,76 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         binding.mapView.getMapAsync(this)
 
-        //Can change mapType.
+        binding.searchView.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return if (query?.isNotEmpty() == true) {
+                    SearchRepository.getGoodRestaurant(query)
+                        .enqueue(object : Callback<SearchResult> {
+                            override fun onResponse(
+                                call: Call<SearchResult>,
+                                response: Response<SearchResult>
+                            ) {
+                                val searchItemList = response.body()?.items.orEmpty()
 
-        //객체
-        SearchRepository.getGoodRestaurant("서울").enqueue(object: Callback<SearchResult>{
-            override fun onResponse(call: Call<SearchResult>, response: Response<SearchResult>) {
-                Log.e("Response","${response.body().toString()}")
+                                if (searchItemList.isEmpty()) {
+                                    Toast.makeText(
+                                        this@MainActivity,
+                                        "검색 결과 없음",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    Log.e("searchItemList","검색 결과 없음")
+
+                                    return
+                                }
+                                // 맵이 초기화가 되지 않았다면
+                                else if (isMapInit.not()) {
+                                    Toast.makeText(this@MainActivity, "오류 발생", Toast.LENGTH_SHORT)
+                                        .show()
+                                    Log.e("isMapInit","오류 발생")
+
+                                    return
+                                }
+
+                                //marker
+                                val markers = searchItemList.map {
+                                    Marker(
+                                        Tm128(
+                                            it.mapx.toDouble(),
+                                            it.mapy.toDouble()
+                                        ).toLatLng()
+                                    ).apply {
+                                        captionText = it.title
+                                        map = naverMap // 초기화가 끝난 후에
+                                    }
+                                }
+
+                                Log.e("markers",markers.toString())
+
+                                // move camera to the first result
+                                // markers.first().position : non-nullable, already checked
+                                val cameraUpdate = CameraUpdate.scrollTo(markers.first().position)
+                                    .animate(CameraAnimation.Easing)
+                                naverMap.moveCamera(cameraUpdate)
+                            }
+
+                            override fun onFailure(call: Call<SearchResult>, t: Throwable) {
+                                TODO("Not yet implemented")
+                            }
+
+                        })
+                    false // 키보드가 내려감
+                } else {
+                    true // 검색어가 없음, 키보드 안 내려감, 사용중
+                }
             }
 
-            override fun onFailure(call: Call<SearchResult>, t: Throwable) {
-                TODO("Not yet implemented")
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
             }
 
         })
+
+        //Can change mapType.
     }
 
     override fun onStart() {
@@ -81,10 +141,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         naverMap = mapObject
         isMapInit = true
 
-        //can use move camera...etc, after getting object here.
-        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.2974, 126.8356))
-            .animate(CameraAnimation.Easing)
-        naverMap.moveCamera(cameraUpdate)
+//        //can use move camera...etc, after getting object here.
+//        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.2974, 126.8356))
+//            .animate(CameraAnimation.Easing)
+//        naverMap.moveCamera(cameraUpdate)
 
     }
 }
