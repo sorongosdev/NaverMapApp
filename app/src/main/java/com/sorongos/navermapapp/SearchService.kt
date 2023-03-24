bda81dbf24790c9f7785126513b370295e77c2f9
Sorongos <dosora9@gmail.com>
2023-03-24 16:18:27 +0900

    쿼리를 받는 서비스 생성

--- app/src/main/java/com/sorongos/navermapapp/SearchService.kt
+++ app/src/main/java/com/sorongos/navermapapp/SearchService.kt
@@ -1,4 +1,15 @@
 package com.sorongos.navermapapp
 
-class SearchService {
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**쿼리를 받는 서비스*/
+interface SearchService {
+
+    @GET("v1/search/local.json")
+    fun getGoodRestaurant(
+        @Query("query") query: String,
+        @Query("display") display: Int, // 표시되는 개수
+    ): Call<SearchResult>
 }
